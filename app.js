8366f70a9b4cb295b79ee937ca117eba8791b399
Federico Fuso <federico.fuso@yahoo.it>
2021-09-13 13:46:25 +0200

    pagine index e show
    ....

--- app.js
+++ app.js
@@ -6,3 +6,14 @@ let sidebar = document.querySelector('.sidebar-desktop');
 
 
 sidebar.setAttribute('style', 'top:' + height + 'px');
+
+function myFunction() {
+   var x = document.getElementById("myDIV");
+   if (x.style.display === "none") {
+      x.style.display = "block";
+   } else {
+      x.style.display = "none";
+   }
+}
+
+// <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
