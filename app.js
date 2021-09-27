76ce2858990d2bf8da72133e04171feee0c2d386
Federico Fuso <federico.fuso@yahoo.it>
2021-09-27 11:29:07 +0200

    ...
    ...

--- app.js
+++ app.js
@@ -21,4 +21,6 @@ function myFunction() {
    }
 }
 
+var i = document.getElementById('myDIV');
+i.style.display='none';
 // <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
